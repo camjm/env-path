e0ab1e88c406f998d503ffc54dc5d3c6cb8d366b
Cam <cam@mackay.net.nz>
2017-06-04 09:18:32 +1200

    use standard instead of eslint

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
 // Imports
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const pathVar = (callback) => {
-  var paths = pathVariable();
-  return Promise.all(paths.map(pathStats)).then(function(array) {
-    callback(null, array);
-    return array;
-  }).catch(function(err){
-    callback(err);
-    return err;
-  });
-};
+  var paths = pathVariable()
+  return Promise.all(paths.map(pathStats)).then(function (array) {
+    callback(null, array)
+    return array
+  }).catch(function (err) {
+    callback(err)
+    return err
+  })
+}
 
 const pathStats = (item) => {
   return new Promise((resolve) => {
@@ -23,36 +23,36 @@ const pathStats = (item) => {
         exists: !err,
         isFile: stats && stats.isFile(),
         isDirectory: stats && stats.isDirectory()
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
 
 const pathVariable = () => {
-  const pathVar = process.env.PATH;
+  const pathVar = process.env.PATH
   if (!pathVar) {
-    throw new Error('PATH environment variable not set');
+    throw new Error('PATH environment variable not set')
   }
-  var paths = pathVar.split(path.delimiter);
-  return paths;
-};
+  var paths = pathVar.split(path.delimiter)
+  return paths
+}
 
 const pathextVariable = () => {
-  const pathextVar = process.env.PATHEXT;
+  const pathextVar = process.env.PATHEXT
   if (!pathextVar) {
-    throw new Error('PATHEXT environment variable not set');
+    throw new Error('PATHEXT environment variable not set')
   }
-  var pathexts = pathextVar.split(path.delimiter);
-  return pathexts;
-};
+  var pathexts = pathextVar.split(path.delimiter)
+  return pathexts
+}
 
 const pathExecutables = (path) => {
-  //TODO:
-};
+  // TODO:
+}
 
 // Expose
-module.exports.pathVar = pathVar;
-module.exports.pathStats = pathStats;
-module.exports.pathVariable = pathVariable;
-module.exports.pathextVariable = pathextVariable;
-module.exports.pathExecutables = pathExecutables;
+module.exports.pathVar = pathVar
+module.exports.pathStats = pathStats
+module.exports.pathVariable = pathVariable
+module.exports.pathextVariable = pathextVariable
+module.exports.pathExecutables = pathExecutables
--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict'
 
-const util = require('util');
-const envPath = require('./index');
+const util = require('util')
+const envPath = require('./index')
 
-var promise = envPath.pathVar(function(err, result) {
-  if (err) console.log('callback has err!');
-  if (result) console.log('callback has result!');
-});
+var promise = envPath.pathVar(function (err, result) {
+  if (err) console.log('callback has err!')
+  if (result) console.log('callback has result!')
+})
 
 promise.then((array) => {
-  console.log('promise has array!');
+  console.log('promise has array!')
   console.log(util.inspect(array, {
     showHidden: true,
     colors: true,
     depth: null
-  }));
+  }))
 }).catch((err) => {
-  console.log('promise has err!');
+  console.log('promise has err!')
   console.log(util.inspect(err, {
     showHidden: true,
     colors: true,
     depth: null
-  }));
-});
+  }))
+})
